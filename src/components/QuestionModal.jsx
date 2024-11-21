6ec0f196848a99c7626db72e16da9b738a7be2ed
Shahbaz-Tanveer <shanakaprince@gmail.com>
2024-11-21 10:45:28 +0530

    modified QuestionModal.jsx in components

--- src/components/QuestionModal.jsx
+++ src/components/QuestionModal.jsx
@@ -5,20 +5,29 @@ const QuestionModal = ({ isOpen, closeModal, question, meaning }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white dark:bg-[#2A2727] p-6 rounded-lg shadow-lg w-11/12 max-w-lg">
-        <div className="flex justify-between items-center mb-4">
-          <h3 className="text-2xl text-[#2851a3] dark:text-[#ffffff] font-semibold">
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center h-[100vh]">
+      <div className="px-4 bg-[#DCE9FD] dark:bg-[#4A4B4A] p-6 rounded-lg shadow-lg w-11/12 max-w-lg">
+        <div className="flex justify-between items-center mb-6">
+          <h3 className="text-3xl text-[#2851a3] dark:text-[#000000] font-bold md:text-4xl">
             {question}
           </h3>
+
           <button
             onClick={closeModal}
-            className="p-2 rounded-full hover:bg-gray-200 transition"
+            className="p-1 rounded-full hover:bg-blue-200 transition hover:dark:bg-slate-500 ml-4"
           >
-            <FiChevronUp className="text-[#2851a3] dark:text-[#ffffff]" />
+            <div className="w-14 h-14 bg-[#ffffff] dark:bg-[#2A2727] rounded-full flex items-center justify-center">
+              <FiChevronUp className="text-[#2851a3] dark:text-[#ffffff] text-4xl" />
+            </div>
           </button>
         </div>
-        <p className="text-lg text-gray-700 dark:text-white">{meaning}</p>
+
+        {/* Meaning Section */}
+        <div className="p-4 bg-white rounded-lg dark:bg-[#a6a6a6] mb-4">
+          <p className="text-xl text-gray-700 font-bold text-[#2851a3] dark:text-[#ffffff] md:text-2xl lg:text-3xl">
+            {meaning}
+          </p>
+        </div>
       </div>
     </div>
   );
